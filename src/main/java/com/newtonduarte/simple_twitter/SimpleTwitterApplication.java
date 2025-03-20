c32d3e90a235dd0b8421185a17df6a5aed2925d5
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-20 15:37:37 -0300

    chore: add initial project files

---
+++ src/main/java/com/newtonduarte/simple_twitter/SimpleTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.newtonduarte.simple_twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newtonduarte/simple_twitter/SimpleTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newtonduarte.simple_twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
