f8382d1be12b2c0647e2c52bf623e8e656a5b3ac
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-20 17:58:51 -0300

    feat: add tweet controller

---
+++ src/main/java/com/newtonduarte/simple_twitter/controllers/TweetController.java
@@ -0,0 +1,63 @@
+package com.newtonduarte.simple_twitter.controllers;
+
+import com.newtonduarte.simple_twitter.entities.Role;
+import com.newtonduarte.simple_twitter.entities.Tweet;
+import com.newtonduarte.simple_twitter.entities.dto.CreateTweetDto;
+import com.newtonduarte.simple_twitter.repositories.TweetRepository;
+import com.newtonduarte.simple_twitter.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/tweets")
+public class TweetController {
+    private final TweetRepository tweetRepository;
+    private final UserRepository userRepository;
+
+    public TweetController(TweetRepository tweetRepository, UserRepository userRepository) {
+        this.tweetRepository = tweetRepository;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createTweet(
+            @RequestBody CreateTweetDto createTweetDto,
+            JwtAuthenticationToken token
+    ) {
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+
+        var tweet = new Tweet();
+        tweet.setUser(user.get());
+        tweet.setContent(createTweetDto.content());
+
+        tweetRepository.save(tweet);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteTweet(
+            @PathVariable Long id,
+            JwtAuthenticationToken token
+    ) {
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        var isAdmin = user.get().getRoles()
+                .stream()
+                .anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+        var tweet = tweetRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (isAdmin || tweet.getUser().getId().equals(UUID.fromString(token.getName()))) {
+            tweetRepository.deleteById(id);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/newtonduarte/simple_twitter/entities/dto/CreateTweetDto.java
@@ -0,0 +1,6 @@
+package com.newtonduarte.simple_twitter.entities.dto;
+
+public record CreateTweetDto(
+        String content
+) {
+}
